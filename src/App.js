9e0e87942fbc9854930102dc458402dfcaea2369
Brad Rogers <brogers@dropbox.com>
2023-02-14 01:04:09 -0500

    Add support for adsense - Add in react-meta-tags for header scripts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import './styles/app.scss';
 import { StretchApp } from './components/app';
+import { MetaTags } from 'react-meta-tags';
 
 export default function App() {
 
   return (
     <div className="App">
+      <MetaTags>
+        <title>stretchy.ninja</title>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4112999267280161" crossOrigin="anonymous"></script>
+      </MetaTags>
       <StretchApp />
     </div>
   );
